9c73d0e3ecd2b45d4e7f61a28137dc630c26db87
Juan Contreras <69876774+contrer-ju@users.noreply.github.com>
2021-10-22 12:20:05 -0400

    Add new file to comply with github policies.

---
+++ src/constants/GithubToken.js
@@ -0,0 +1,6 @@
+export const githubToken =
+"";
+
+// Replace the value of the constant with a valid token from GitHub
+// for the correct functioning of the APP, for security reasons you
+// cannot place tokens in files stored on GitHub. 
