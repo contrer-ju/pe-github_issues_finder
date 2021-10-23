d61b266c3a2e797e2bb40be9149874c39a593fb2
Juan Contreras <69876774+contrer-ju@users.noreply.github.com>
2021-10-23 15:28:02 -0400

    Update file according to GitHub policies

--- src/constants/GithubToken.js
+++ src/constants/GithubToken.js
@@ -1,10 +1,5 @@
-export const githubToken =
-<<<<<<< HEAD
-"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";
-=======
-"";
+export const githubToken ="";
 
 // Replace the value of the constant with a valid token from GitHub
 // for the correct functioning of the APP, for security reasons you
-// cannot place tokens in files stored on GitHub. 
->>>>>>> 9c73d0e3ecd2b45d4e7f61a28137dc630c26db87
+// cannot place tokens in files stored on GitHub.
